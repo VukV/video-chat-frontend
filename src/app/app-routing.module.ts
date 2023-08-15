81e91c6318c5873cac9b9f34fcb4051ea732ac2a
VukV <vuk.vukovic.2000@gmail.com>
2023-08-15 18:30:32 +0200

    guard token validation + minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from "./components/main/main.component";
 import {LoginComponent} from "./components/login/login.component";
+import {loginGuard} from "./guards/login.guard";
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    canActivate: [loginGuard]
   },
   {
     path: 'login',
--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,13 +1,31 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {CurrentUserService} from "../services/current-user.service";
 import {inject} from "@angular/core";
+import jwtDecode from "jwt-decode";
+import {JwtPayload} from "../model/jwt-payload";
 
 export const loginGuard: CanActivateFn = (route, state) => {
   const currentUserService: CurrentUserService = inject(CurrentUserService);
   const router: Router = inject(Router);
 
+  //return true;
+
   let token = currentUserService.getToken();
-  //todo verify token
+  if(!token){
+    router.navigate(['/login']);
+    return false;
+  }
+
+  let decoded = jwtDecode(token);
+  if(!isJwt(decoded)){
+    router.navigate(['/login']);
+    return false;
+  }
 
   return true;
 };
+
+
+const isJwt = (token: any): token is JwtPayload => {
+  return typeof token === 'object' && Boolean(token.userId && token.sub);
+}
