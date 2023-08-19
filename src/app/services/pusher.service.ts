ec328e5cff22532123e0e55a53a131e25f32ae3f
VukV <vuk.vukovic.2000@gmail.com>
2023-08-19 16:25:06 +0200

    pusher env

--- src/app/services/pusher.service.ts
+++ src/app/services/pusher.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import Pusher from "pusher-js";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class PusherService {
   private pusher: any;
 
   constructor() {
-    this.pusher = new Pusher("", {
-      cluster: ''
+    this.pusher = new Pusher(environment.pusher.key, {
+      cluster: environment.pusher.cluster
     })
   }
 }
